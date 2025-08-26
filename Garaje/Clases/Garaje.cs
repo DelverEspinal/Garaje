ccdd5c85201ae1a74664e263d657cc33ff9c6bce
DelverEspinal <delvervarela66@gmail.com>
2025-08-25 21:58:19 -0600

    Proyecto Garaje

---
+++ Garaje/Clases/Garaje.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garaje.Clases
+{
+    public class Garaje
+    {
+        private int numeroGaraje {  get; set; }
+        private string descripcion {  get; set; }
+        private int capacidadEspacios { get; set; }
+        private int espaciosOcupados { get; set; }
+
+
+        public Garaje(int numeroGaraje, string descripcion, int capacidadEspacios, int espaciosOcupados)
+        {
+            this.numeroGaraje = numeroGaraje;
+            this.descripcion = descripcion;
+            this.capacidadEspacios = capacidadEspacios;
+            this.espaciosOcupados = espaciosOcupados;
+        }
+
+        //metodo para retornar espacios disponibles
+
+        public int getcapacidadEspacios()
+        {
+            return capacidadEspacios;
+        }
+
+        public int getespaciosOcupados()
+        {
+            return espaciosOcupados;
+        }
+
+
+        public int getespaciosDisponibles()
+        {
+            return capacidadEspacios - espaciosOcupados;
+        }
+
+        public void actualizarEspaciosOcupados(int cuposSolicitados)
+        {
+            espaciosOcupados = espaciosOcupados - cuposSolicitados;
+        }
+    }
+}
---
+++ Garaje/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Garaje.Clases;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garaje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Garaje g1 = new Garaje(1, "Garaje Central", 50, 10);
+            Garaje g1;
+
+            Console.WriteLine("Espacios disponibles: " + g1.getEspaciosDisponibles());
+
+            g1.actualizarEspaciosOcupados(5);
+
+            Console.WriteLine("Espacios ocupados: " + g1.getespaciosOcupados());
+            Console.WriteLine("Espacios disponibles: " + g1.getEspaciosDisponibles());
+        }
+    }
+}
